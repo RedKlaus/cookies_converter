e60988287333a06c65c65a51fcfbebb3fa9f85d3
REDKLAUS <reallifestd@gmail.com>
2024-10-07 13:30:31 +0300

    update version name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='cookies_converter',
-    version='0.3.1',
+    version='0.3.2',
     author='REDKLAUS',
     author_email='reallifestd@gmail.com',
     long_description=readme(),
